39e4f594255f3c945393e02cdb856658707ada26
SpencerDodd <spencer.evan.dodd@gmail.com>
2016-04-11 18:58:48 -0400

    New module containing methods for requests over the internet

---
+++ src/LocalCrossBlast/NetworkRequests.py
@@ -0,0 +1,16 @@
+"""
+This module contains a number of methods that handle requests over the
+internet. This includes:
+	- Phylogenetic lookups
+	- NCBI requests (phylogeny, accession, etc.)
+	- other ...
+"""
+class NetworkRequests:
+
+	"""
+	Looks up a sequence by a full Fasta name and returns the accession number
+	of the given string name
+	"""
+	def lookup_accession(self, request_name):
+
+		pass
